5c67d0974dd0e96bc4fa4e87114d3f5e6cb64894
Hafiz Aina <hafizaina94@gmail.com>
2016-04-17 19:57:36 -0400

    user registration forms are up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'home/front'
+  root to: 'home#front'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
